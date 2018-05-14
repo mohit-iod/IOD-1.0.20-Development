01c2c17925ebeeb6dde018128a193dd94bbd531a
mohit-iod <mohit91.iod@gmail.com>
2018-05-14 11:25:54 +0530

    Explore View and TableCell

---
+++ iOnlineDoctor/iOnlineDoctor/ExploreCell.h
@@ -0,0 +1,16 @@
+//
+//  ExploreCell.h
+//  iOnlineDoctor
+//
+//  Created by Deepak Patel on 5/14/18.
+//  Copyright © 2018 iOnlineDoctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExploreCell : UITableViewCell
+
+@property(nonatomic,strong)IBOutlet UIImageView * imgView;
+@property(nonatomic,strong)IBOutlet UILabel * lblText;
+
+@end
---
+++ iOnlineDoctor/iOnlineDoctor/ExploreCell.m
@@ -0,0 +1,24 @@
+//
+//  ExploreCell.m
+//  iOnlineDoctor
+//
+//  Created by Deepak Patel on 5/14/18.
+//  Copyright © 2018 iOnlineDoctor. All rights reserved.
+//
+
+#import "ExploreCell.h"
+
+@implementation ExploreCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
